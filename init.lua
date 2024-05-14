0d9256309738d6c268d13eb365ce7a0eb521305a
Adam Byrne <adamrbyrne@gmail.com>
2024-05-14 09:45:16 +0100

    hmmm

--- init.lua
+++ init.lua
@@ -74,7 +74,20 @@ require("lazy").setup({
 			"MunifTanjim/nui.nvim",
 		},
 	},
-
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
 	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
 		opts = {
@@ -402,8 +415,6 @@ require("lazy").setup({
 		"folke/tokyonight.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme "torte"
-
 			vim.cmd.hi "Comment gui=none"
 		end,
 	},
