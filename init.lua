7132fa6278141839a5f61f4ff0695b15a9c72b1f
Adam Byrne <adamrbyrne@gmail.com>
2024-06-26 22:35:22 +0100

    chore: missing ,

--- init.lua
+++ init.lua
@@ -120,7 +120,7 @@ require("lazy").setup({
   event = {"CmdlineEnter"},
   ft = {"go", 'gomod'},
   build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-}
+},
 	{
 		"lewis6991/gitsigns.nvim",
 		opts = {
