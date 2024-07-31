8c7c28149ab04e5148be7b7e145720c0e99f6033
Adam Byrne <adamrbyrne@gmail.com>
2024-07-31 23:42:35 +0100

    Updates

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,6 +33,54 @@ require("lazy").setup({
 		dependencies = { "MunifTanjim/nui.nvim" },
 	},
 	{
+		"goerz/jupytext.vim",
+		ft = { "ipynb" },
+		build = "pip install jupytext",
+		config = function()
+			vim.g.jupytext_fmt = "py"
+			vim.g.jupytext_style = "hydrogen"
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"GCBallesteros/NotebookNavigator.nvim",
+		keys = {
+			{
+				"]h",
+				function()
+					require("notebook-navigator").move_cell "d"
+				end,
+			},
+			{
+				"[h",
+				function()
+					require("notebook-navigator").move_cell "u"
+				end,
+			},
+			{ "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+			{ "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+		},
+		dependencies = {
+			"echasnovski/mini.comment",
+			"hkupty/iron.nvim", -- repl provider
+			-- "akinsho/toggleterm.nvim", -- alternative repl provider
+			-- "benlubas/molten-nvim", -- alternative repl provider
+			"anuvyklack/hydra.nvim",
+		},
+		event = "VeryLazy",
+		config = function()
+			local nn = require "notebook-navigator"
+			nn.setup { activate_hydra_keys = "<leader>h" }
+		end,
+	},
+	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
