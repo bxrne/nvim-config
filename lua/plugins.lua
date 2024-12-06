7153299f03c6b6e6b7cece4f71948627365a747f
Adam Byrne <adam.r.byrne@gmail.com>
2024-12-06 16:17:22 +0000

    dash

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,6 +18,18 @@ require("lazy").setup {
 		},
 	},
 	{
+		"goolord/alpha-nvim",
+		-- dependencies = { 'echasnovski/mini.icons' },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local startify = require "alpha.themes.startify"
+			-- available: devicons, mini, default is mini
+			-- if provider not loaded and enabled is true, it will try to use another provider
+			startify.file_icons.provider = "devicons"
+			require("alpha").setup(startify.config)
+		end,
+	},
+	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		config = function()
