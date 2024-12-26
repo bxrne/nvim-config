0796bc96baf85eb8b9f319d277e799221c5a7226
Adam Byrne <adam.r.byrne@gmail.com>
2024-12-26 00:05:06 +0000

    refactor lualine configuration: remove bubbles.lua and integrate settings directly in plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -315,10 +315,36 @@ require("lazy").setup {
 	},
 	{
 		'nvim-lualine/lualine.nvim',
-		config = function()
-			require('custom/bubbles')
-		end,
+		opts = {
+	 options = {
+    theme = "rose-pine",
+    component_separators = '',
+    section_separators = { left = '', right = '' },
+  },
+		sections = {
+    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+    lualine_b = { 'filename', 'branch' },
+    lualine_c = {
+      '%=', --[[ add your center compoentnts here in place of this comment ]]
+    },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
 	},
+},
 	{
 		"lervag/vimtex",
 		config = function()
