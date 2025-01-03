b72007830281489c4ca41f2837ad13ad4b02b4ec
Adam Byrne <adam.r.byrne@gmail.com>
2025-01-03 21:21:45 +0000

    idk

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,59 +1,60 @@
 local M = {}
 
 function M.setup()
-    local cmp = require('cmp')
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	local cmp = require "cmp"
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    require("mason").setup()
-    require("mason-lspconfig").setup()
+	require("mason").setup()
+	require("mason-lspconfig").setup()
 
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                require('luasnip').lsp_expand(args.body)
-            end,
-        },
-        mapping = cmp.mapping.preset.insert({
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-        }, {
-            { name = 'buffer' },
-        })
-    })
+	cmp.setup {
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert {
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm { select = true },
+		},
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
+		}, {
+			{ name = "buffer" },
+		}),
+	}
 
-    -- Configure LSP servers
-    local lspconfig = require('lspconfig')
-    
-    -- Go
-    lspconfig.gopls.setup({
-        capabilities = capabilities,
-        settings = {
-            gopls = {
-                analyses = {
-                    unusedparams = true,
-                },
-                staticcheck = true,
-            },
-        },
-    })
+	-- Configure LSP servers
+	local lspconfig = require "lspconfig"
 
-    -- Global LSP mappings
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover documentation' })
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Go to implementation' })
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { desc = 'Signature help' })
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, { desc = 'Add workspace folder' })
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, { desc = 'Remove workspace folder' })
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename' })
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
+	-- Go
+	lspconfig.gopls.setup {
+		capabilities = capabilities,
+		settings = {
+			gopls = {
+				analyses = {
+					unusedparams = true,
+				},
+				staticcheck = true,
+			},
+		},
+	}
+
+	-- Global LSP mappings
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+	vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" })
+	vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder" })
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 end
 
 return M
