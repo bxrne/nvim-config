f9c96a7eab250c9bf8949514050ee227a3ee6ad6
Adam Byrne <adam.r.byrne@gmail.com>
2025-02-18 21:44:02 +0000

    follow dir

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -153,6 +153,9 @@ require("lazy").setup {
 						--						theme = "ivy",
 						-- disables netrw and use telescope-file-browser in its place
 						hijack_netrw = true,
+						cwd_to_path = true,
+						hidden = { file_browser = true, folder_browser = true },
+
 						mappings = {
 							["i"] = {
 								-- your custom insert mode mappings
